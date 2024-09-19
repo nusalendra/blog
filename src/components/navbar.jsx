5858291f0e06b3db71fe478322bffe6c9034d36f
Nusalendra <nusalendraa@gmail.com>
2024-09-20 00:10:38 +0700

    feat:create a blog with the title konfigurasi email pada laravel 10

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -81,7 +81,7 @@ function Navbar() {
               </li>
               <li>
                 <Link
-                  href="#"
+                  href="/blog"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-white dark:hover:text-black md:dark:hover:bg-transparent"
                 >
                   Blog
